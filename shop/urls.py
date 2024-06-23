7085191974306087e33dbf05c3d61bcca3aa8b57
Tetiana Yuriivna Berlizova <tebe@enabl.dk>
2024-06-23 11:58:37 +0300

    all_staff, all_news

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
-from .views import shop_view, product_detail, news_detail, staff_detail, add_to_cart
+from .views import (
+    shop_view,
+    product_detail,
+    news_detail,
+    staff_detail,
+    add_to_cart,
+    category_detail,
+    all_news_view,
+    all_staff_view
+)
 
 urlpatterns = [
     path('', shop_view, name='shop'),
@@ -7,4 +16,7 @@ urlpatterns = [
     path('news/<int:pk>/', news_detail, name='news_detail'),
     path('staff/<int:pk>/', staff_detail, name='staff_detail'),
     path('add_to_cart/<int:pk>/', add_to_cart, name='add_to_cart'),
+    path('category/<int:pk>/', category_detail, name='category_detail'),
+    path('all_news/', all_news_view, name='all_news'),
+    path('all_staff/', all_staff_view, name='all_staff'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,31 +2,43 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import ProdCategory, Prod, Contacts, Staff, News
 from django.views.decorators.http import require_POST
 
+
 def shop_view(request):
     categories = ProdCategory.objects.filter(is_visible=True)
-    products = Prod.objects.filter(is_visible=True)
-    staff = Staff.objects.filter(is_visible=True)
+    categories_with_limited_products = []
+    for category in categories:
+        limited_products = category.prods.filter(is_visible=True)[:3]
+        categories_with_limited_products.append((category, limited_products))
+
+    staff = Staff.objects.filter(is_visible=True)[:3]
+    all_staff = Staff.objects.filter(is_visible=True)
     contacts = Contacts.objects.all()
-    news = News.objects.all()
+    news = News.objects.all()[:3]
+    all_news = News.objects.all()
     return render(request, 'shop/shop.html', {
-        'categories': categories,
-        'products': products,
+        'categories_with_limited_products': categories_with_limited_products,
         'staff': staff,
+        'all_staff': all_staff,
         'contacts': contacts,
         'news': news,
+        'all_news': all_news,
     })
+
 
 def product_detail(request, pk):
     product = get_object_or_404(Prod, pk=pk)
     return render(request, 'shop/product_detail.html', {'product': product})
 
+
 def news_detail(request, pk):
     news_item = get_object_or_404(News, pk=pk)
     return render(request, 'shop/news_detail.html', {'news_item': news_item})
 
+
 def staff_detail(request, pk):
     staff_member = get_object_or_404(Staff, pk=pk)
     return render(request, 'shop/staff_detail.html', {'staff_member': staff_member})
+
 
 @require_POST
 def add_to_cart(request, pk):
@@ -39,3 +51,22 @@ def add_to_cart(request, pk):
         cart[pk] = quantity
     request.session['cart'] = cart
     return redirect('shop')
+
+
+def category_detail(request, pk):
+    category = get_object_or_404(ProdCategory, pk=pk)
+    products = category.prods.filter(is_visible=True)
+    return render(request, 'shop/category_detail.html', {
+        'category': category,
+        'products': products,
+    })
+
+
+def all_news_view(request):
+    news = News.objects.all()
+    return render(request, 'shop/all_news.html', {'news': news})
+
+
+def all_staff_view(request):
+    staff = Staff.objects.all()
+    return render(request, 'shop/all_staff.html', {'staff': staff})
