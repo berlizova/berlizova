19cacbdca8b71982a5ecf0fc491dd8663bedd3e3
Tetiana Yuriivna Berlizova <tebe@enabl.dk>
2024-06-23 10:59:48 +0300

    add admin

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import shop_view
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', shop_view, name='shop'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import ProdCategory, Prod, Contacts, Staff, News
 
 
-def index(request):
-    return render(request, 'shop/shop.html')
+def shop_view(request):
+    categories = ProdCategory.objects.filter(is_visible=True)
+    products = Prod.objects.filter(is_visible=True)
+    staff = Staff.objects.filter(is_visible=True)
+    contacts = Contacts.objects.all()
+    news = News.objects.all()
+    return render(request, 'shop/shop.html', {
+        'categories': categories,
+        'products': products,
+        'staff': staff,
+        'contacts': contacts,
+        'news': news,
+    })
