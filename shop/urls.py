c857eff762393c0e96866577c30540fd57448ec8
Tetiana Yuriivna Berlizova <tebe@enabl.dk>
2024-06-23 11:42:21 +0300

    detail html

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import shop_view
+from .views import shop_view, product_detail, news_detail, staff_detail, add_to_cart
 
 urlpatterns = [
     path('', shop_view, name='shop'),
+    path('product/<int:pk>/', product_detail, name='product_detail'),
+    path('news/<int:pk>/', news_detail, name='news_detail'),
+    path('staff/<int:pk>/', staff_detail, name='staff_detail'),
+    path('add_to_cart/<int:pk>/', add_to_cart, name='add_to_cart'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import ProdCategory, Prod, Contacts, Staff, News
-
+from django.views.decorators.http import require_POST
 
 def shop_view(request):
     categories = ProdCategory.objects.filter(is_visible=True)
@@ -15,3 +15,27 @@ def shop_view(request):
         'contacts': contacts,
         'news': news,
     })
+
+def product_detail(request, pk):
+    product = get_object_or_404(Prod, pk=pk)
+    return render(request, 'shop/product_detail.html', {'product': product})
+
+def news_detail(request, pk):
+    news_item = get_object_or_404(News, pk=pk)
+    return render(request, 'shop/news_detail.html', {'news_item': news_item})
+
+def staff_detail(request, pk):
+    staff_member = get_object_or_404(Staff, pk=pk)
+    return render(request, 'shop/staff_detail.html', {'staff_member': staff_member})
+
+@require_POST
+def add_to_cart(request, pk):
+    product = get_object_or_404(Prod, pk=pk)
+    quantity = int(request.POST.get('quantity', 1))
+    cart = request.session.get('cart', {})
+    if pk in cart:
+        cart[pk] += quantity
+    else:
+        cart[pk] = quantity
+    request.session['cart'] = cart
+    return redirect('shop')
