1bb43037e91e9ac28bfde281eaeb5afb89e02777
Tetiana Yuriivna Berlizova <tebe@enabl.dk>
2024-06-27 09:32:06 +0300

    local host*

--- berlizova/settings.py
+++ berlizova/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['berlizova-diplom.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['berlizova-diplom.herokuapp.com', 'localhost', '127.0.0.1', '*']
 
 # Application definition
 INSTALLED_APPS = [
